8ffc8daf793af8afa0b21085dac4847059f27898
Matt <m22mail@comcast.net>
2023-07-22 23:27:54 -0400

    Add minimax structure

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,12 +1,62 @@
 ﻿using ChessChallenge.API;
+using System;
 using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
+	Board _board;
+	int[] moveValues = new int[256];
+	bool isWhiteTeam;
+
 	public Move Think(Board board, Timer timer)
 	{
-		Move[] moves = board.GetLegalMoves();
+		_board = board;
+        isWhiteTeam = _board.IsWhiteToMove;
+        Move[] moves = GetLegalMoves();
 
-		return moves[0];
+		//Acquire all the move values through minimax
+		for(int i = 0; i < moves.Length; i++)
+		{
+			moveValues[i] = MiniMaxAbsolute(moves[i], 1, true);
+		}
+		//Find the best value and select it!
+		//Moves that tie with highest are ignored
+		int highestMoveIndex = 0;
+		int highestValue = -40000;
+        for (int i = 0; i < moves.Length; i++)
+        {
+			if (moveValues[i] > highestValue)
+			{
+				highestValue = moveValues[i];
+				highestMoveIndex = i;
+			}
+        }
+        return moves[highestMoveIndex];
+	}
+
+	//MAIN LOGIC
+
+	int MiniMaxAbsolute(Move currentMove, int depth, bool thisTeamTurn)
+	{
+		if(depth == 0)
+		{
+			//Count up number of pieces we have left
+			ulong bitboardToUse = _board.BlackPiecesBitboard;
+			if(isWhiteTeam)
+			{
+				bitboardToUse = _board.WhitePiecesBitboard;
+			}
+			return 0;
+		}
+
+		return 0;
+	}
+
+
+	//HELPERS
+
+	Move[] GetLegalMoves()
+	{
+		return _board.GetLegalMoves();
 	}
 }
